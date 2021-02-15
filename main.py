210c06dfa88e9f4a6ab7ece7f7ff0e0cea48cd3d
Krittika-Saha <krittika.saha.dev@gmail.com>
2021-02-15 10:31:51 +0530

    Completed Project :D
    Planning on adding some other stuff later...

---
+++ main.py
@@ -0,0 +1,66 @@
+import requests
+from datetime import datetime
+
+USERNAME = "_YOUR_USERNAME_"
+TOKEN = "_YOUR_TOKEN_"
+GRAPH_ID = "_YOUR_GRAPH_ID_"
+
+pixela_endpoint = "https://pixe.la/v1/users"
+user_params = {
+    "token": TOKEN,
+    "username": USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+}
+
+## POST - CREATING AN USER
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": GRAPH_ID,
+    "name": "Programming Graph",
+    "unit": "min",
+    "type": "float",
+    "color": "ajisai"
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+#POST - MAKING A GRAPH
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)
+
+pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+
+today = datetime.now()
+
+pixel_data = {
+    "date": today.strftime("%Y%m%d"),
+    "quantity": input("How much did you program today? "),
+}
+
+response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
+print(response.text)
+
+update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{input('The date(YYYYMMDD): ')}"
+
+new_pixel_data = {
+    "quantity": input("The amount: ")
+}
+
+## PUT
+# response = requests.put(url=update_endpoint, json=new_pixel_data, headers=headers)
+# print(response.text)
+
+
+delete_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+
+
+## DELETE
+# response = requests.delete(url=delete_endpoint, headers=headers)
+# print(response.text)
